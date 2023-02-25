81cbfbd6979283b4b6d6223c830fc1ca1850b8b2
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-25 21:29:14 +0530

    Login + Create Post Implemented

---
+++ src/mock_data/userList.js
@@ -0,0 +1,20 @@
+const userMap = new Map([
+  [
+    "abhideepghosh@gmail.com",
+    { id: 1, name: "Abhideep Ghosh", password: "reddit123" },
+  ],
+  [
+    "mehnaz@gmail.com",
+    { id: 2, name: "Mehnaz Ashrafi Parveen", password: "reddit456" },
+  ],
+  [
+    "shabnoor@gmail.com",
+    { id: 3, name: "Shabnoor Ashrafi Parveen", password: "reddit789" },
+  ],
+  [
+    "bivek@gmail.com",
+    { id: 4, name: "Bivek Kumar Shaw", password: "reddit101112" },
+  ],
+]);
+
+export default userMap;
